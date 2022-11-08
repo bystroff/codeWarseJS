03bc248d758f7998f18f7ab3bd16e259d3a5c2fd
Kirill Bystrov <pes13@yandex.ru>
2022-11-08 18:35:18 +0300

    task 6 done

---
+++ 6.js
@@ -0,0 +1,16 @@
+function switchItUp(number){
+  let ans = '';
+  switch (number) {
+      case 0: ans = "Zero"; break;
+      case 1: ans = "One"; break;
+      case 2: ans = "Two"; break;
+      case 3: ans = "Three"; break;
+      case 4: ans = "Four"; break;
+      case 5: ans = "Five"; break;
+      case 6: ans = "Six"; break;
+      case 7: ans = "Seven"; break;
+      case 8: ans = "Eight"; break;
+      case 9: ans = "Nine"; break;
+  }
+  return ans;
+}
