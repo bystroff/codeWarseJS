da3c6ce0af409d0edc9cc2110162a5dfb387f47f
Kirill Bystrov <pes13@yandex.ru>
2022-11-10 14:42:35 +0300

    task welcome done

---
+++ js/welcome.js
@@ -0,0 +1,29 @@
+function greet(language) {
+	let db = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+  };
+  
+  for (let lang in db) {
+    if (lang == language) {
+      return db[lang]; 
+    }
+  }
+  return db['english'];
+  
+}
